a8eaa327b00d0b599a9c1a92f176cdfff592eedc
jiang <392711804@qq.com>
2020-08-11 17:29:03 +0800

    merge unp_study

---
+++ compile/mymath.i
@@ -0,0 +1,24 @@
+# 1 "mymath.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "mymath.c"
+
+
+
+
+# 1 "mymath.h" 1
+
+
+
+
+
+
+int add(int a, int b);
+# 6 "mymath.c" 2
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ compile/mymath.s
@@ -0,0 +1,25 @@
+	.file	"mymath.c"
+	.text
+	.globl	add
+	.type	add, @function
+add:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add, .-add
+	.ident	"GCC: (Ubuntu 9.3.0-11ubuntu0~18.04.1) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
